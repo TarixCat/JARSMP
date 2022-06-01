6f7446d1880d0976d743d7e7bf323d2dfee13818
TarixCat <mrcatter05@gmail.com>
2022-06-01 20:55:25 +1000

    More custom recipes

--- src/main/java/me/devtarix/jarsmp/registries/ShapedRecipeProvider.java
+++ src/main/java/me/devtarix/jarsmp/registries/ShapedRecipeProvider.java
@@ -1,9 +1,26 @@
 package me.devtarix.jarsmp.registries;
 
+import me.devtarix.jarsmp.recipes.EmeraldBlade;
 import me.devtarix.jarsmp.recipes.EnchantedBook;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+import static me.devtarix.jarsmp.util.handles.Logger.print;
 
 public class ShapedRecipeProvider {
+    public static HashMap<String, ItemStack> recipeRegistry = new HashMap<>();
+
+    public void test() {
+        recipeRegistry.forEach((namespacedKey, itemStack) -> print("Registered: " + namespacedKey));
+    }
     public ShapedRecipeProvider() {
-        EnchantedBook EB = new EnchantedBook();
+        init();
+        test();
+    }
+
+    public void init() {
+        new EnchantedBook();
+        new EmeraldBlade();
     }
 }
