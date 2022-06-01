5f4ab7b6f770883c24d99e62dd8a7e0b44781c29
TarixCat <mrcatter05@gmail.com>
2022-06-01 20:56:04 +1000

    Modified Pigstep.java to be Sounds.java

--- src/main/java/me/devtarix/jarsmp/commands/Sounds.java
+++ src/main/java/me/devtarix/jarsmp/commands/Sounds.java
@@ -7,15 +7,21 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Pigstep implements CommandExecutor {
+public class Sounds implements CommandExecutor {
+
+    private Sound run;
     Notifs n = new Notifs();
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
-            ((Player) sender).playSound(((Player) sender).getLocation() ,Sound.MUSIC_DISC_PIGSTEP, 150, 1);
+            ((Player) sender).playSound(((Player) sender).getLocation() ,run, 150, 1);
         }
         else {
             n.notUser(sender);
         }
         return true;
+    }
+
+    public Sounds(Sound sound) {
+        this.run = sound;
     }
 }
