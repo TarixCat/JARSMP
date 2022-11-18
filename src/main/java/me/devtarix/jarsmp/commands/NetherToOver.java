f2a7bcfde1da900e6fd6d5beef806bd04448d219
TarixCat <mrcatter05@gmail.com>
2022-11-18 22:57:36 +1100

    Converted to using ints and now checks for dimension

--- src/main/java/me/devtarix/jarsmp/commands/NetherToOver.java
+++ src/main/java/me/devtarix/jarsmp/commands/NetherToOver.java
@@ -2,6 +2,7 @@ package me.devtarix.jarsmp.commands;
 
 import me.devtarix.jarsmp.util.CI;
 import me.devtarix.jarsmp.util.handles.Notifs;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,15 +13,18 @@ public class NetherToOver implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            double blockX = player.getPlayer().getLocation().getBlockX();
-            double blockY = player.getPlayer().getLocation().getBlockY();
-            double blockZ = player.getPlayer().getLocation().getBlockZ();
-            double NBlockX = blockX * 8;
-            double NBlockZ = blockZ * 8;
+            if (player.getLocation().getWorld().getEnvironment().equals(World.Environment.NETHER)) {
+                int blockX = player.getPlayer().getLocation().getBlockX();
+                int blockY = player.getPlayer().getLocation().getBlockY();
+                int blockZ = player.getPlayer().getLocation().getBlockZ();
+                int NBlockX = blockX * 8;
+                int NBlockZ = blockZ * 8;
 
-            player.sendMessage(CI.W + "This command does not check what dimension you are in. Run this command in the " + CI.E + "Nether");
-            player.sendMessage(CI.I + "Your current coordinates translated in the Overworld:");
-            player.sendMessage(CI.S + "X= " + NBlockX + " Y= " + blockY + " Z= " + NBlockZ);
+                player.sendMessage(CI.I + "Your current coordinates translated in the Overworld:");
+                player.sendMessage(CI.S + "X= " + NBlockX + " Y= " + blockY + " Z= " + NBlockZ);
+            } else {
+                player.sendMessage(CI.W + "Please run this command in the " + CI.E + "Nether " + CI.W + "for coordinate translation.");
+            }
         }
         else {
             n.notUser(sender);
