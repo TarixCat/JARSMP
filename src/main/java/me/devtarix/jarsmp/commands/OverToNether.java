c085ed2d175656c16d400a11cfd3f2d014b2beb1
TarixCat <mrcatter05@gmail.com>
2022-11-28 21:45:23 +1100

    I broke something??

--- src/main/java/me/devtarix/jarsmp/commands/OverToNether.java
+++ src/main/java/me/devtarix/jarsmp/commands/OverToNether.java
@@ -13,7 +13,6 @@ public class OverToNether implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (player.getLocation().getWorld().getEnvironment().equals(World.Environment.NETHER)) {
                 int blockX = player.getPlayer().getLocation().getBlockX();
                 int blockY = player.getPlayer().getLocation().getBlockY();
                 int blockZ = player.getPlayer().getLocation().getBlockZ();
@@ -22,9 +21,6 @@ public class OverToNether implements CommandExecutor {
 
                 player.sendMessage(CI.I + "Your current coordinates translated in the Nether:");
                 player.sendMessage(CI.S + "X= " + NBlockX + " Y= " + blockY + " Z= " + NBlockZ);
-            } else {
-                player.sendMessage(CI.W + "Please run this command in the " + CI.I + "Overworld " + CI.W + "for coordinate translation.");
-            }
         }
         else {
             n.notUser(sender);
