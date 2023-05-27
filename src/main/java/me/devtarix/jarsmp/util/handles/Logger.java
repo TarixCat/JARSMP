e762b127fcd9cffbd2a192ab79c5406dc0f9aff8
TarixCat <mrcatter05@gmail.com>
2023-05-27 23:43:44 +1000

    Added JDoc

--- src/main/java/me/devtarix/jarsmp/util/handles/Logger.java
+++ src/main/java/me/devtarix/jarsmp/util/handles/Logger.java
@@ -2,6 +2,11 @@ package me.devtarix.jarsmp.util.handles;
 
 import java.util.logging.Level;
 
+/**
+ * Logging Methods!!!
+ */
+
+//TODO: Probably could be put into Notifs or put Notifs into here
 public class Logger {
     public static void log(Level level, String msg) {
         System.out.println("[" + level.toString() + "]: " + msg);
