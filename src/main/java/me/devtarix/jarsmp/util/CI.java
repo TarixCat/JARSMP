a83e304e34375a38f25ffa640d46bc6407afad9c
TarixCat <mrcatter05@gmail.com>
2023-05-27 23:41:39 +1000

    Added JDoc and removed unused code

--- src/main/java/me/devtarix/jarsmp/util/CI.java
+++ src/main/java/me/devtarix/jarsmp/util/CI.java
@@ -2,11 +2,13 @@ package me.devtarix.jarsmp.util;
 
 import org.bukkit.ChatColor;
 
+/**
+ * Color Selector Interface for standardization
+ */
 public interface CI {
     ChatColor E = ChatColor.RED;
     ChatColor W = ChatColor.YELLOW;
     ChatColor S = ChatColor.GREEN;
     ChatColor I = ChatColor.AQUA;
-    ChatColor C = ChatColor.DARK_RED;
     ChatColor G = ChatColor.GOLD;
 }
