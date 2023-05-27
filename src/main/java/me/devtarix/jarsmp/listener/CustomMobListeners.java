98a1d4d86b970df7ec62a940e09976fdc27e093b
TarixCat <mrcatter05@gmail.com>
2023-05-27 23:41:54 +1000

    Added JDoc and corrected code

--- src/main/java/me/devtarix/jarsmp/listener/CustomMobListeners.java
+++ src/main/java/me/devtarix/jarsmp/listener/CustomMobListeners.java
@@ -5,14 +5,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
+/**
+ * Separate Listeners to customize mob behavior
+ */
 public class CustomMobListeners implements Listener {
 
+    /**
+     * Reduces the chance of a baby zombie spawning by ((Math.random() * 100) < 50)
+     * @param event
+     */
     @EventHandler
     public void zombieSpawn(EntitySpawnEvent event) {
         if(event.getEntityType().equals(EntityType.ZOMBIE)){
             Ageable a = (Ageable) event.getEntity();
             if(!a.isAdult()) {
-                if (Math.random() * 100 < 50) {
+                if ((Math.random() * 100) < 50) {
                     a.setAdult();
                 }
             }
