15236e93712b1fb7bcb5fceecfa664f7d8b477a8
TarixCat <mrcatter05@gmail.com>
2023-05-27 23:44:45 +1000

    Added JDoc and added inverse method because I could

--- src/main/java/me/devtarix/jarsmp/util/handles/Maths.java
+++ src/main/java/me/devtarix/jarsmp/util/handles/Maths.java
@@ -2,8 +2,31 @@ package me.devtarix.jarsmp.util.handles;
 
 import org.bukkit.util.Vector;
 
+/**
+ * "Useful" maths
+ */
 public class Maths {
+    /**
+     * Used to apply a pull effect on movement (using movement code)
+     * @param x x direction
+     * @param y y direction
+     * @param z z direction
+     * @param speed speed
+     * @return vector
+     */
     public Vector vecpull(double x, double y, double z, double speed) {
         return new Vector(x, y, z).normalize().multiply(-speed);
+    }
+
+    /**
+     * Used to apply a push effect on movement (using movement code)
+     * @param x x direction
+     * @param y y direction
+     * @param z z direction
+     * @param speed speed
+     * @return vector
+     */
+    public Vector vecpush(double x, double y, double z, double speed) {
+        return new Vector(x,y,z).normalize().multiply(speed);
     }
 }
